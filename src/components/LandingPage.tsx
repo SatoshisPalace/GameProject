67443a15c604fba1f639038639e14de09772bcec
Ethan <Ethan.Gold-Steinberg@protonmail.com>
2024-12-16 12:27:58 -0500

    fixed names

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -267,8 +267,8 @@ const LandingPage: React.FC = () => {
         <Suspense fallback={<div>Loading game...</div>}>
           <WalletProvider>
             {selectedGame?.id === 'PONG' && <PongGame gameId={'PONG'} />}
-            {selectedGame?.id === 'TETRIS' && <TetrisGame gameId={'TETRIS'} />}
-            {selectedGame?.id === 'SATOSHI_MAN' && <SatoshiManGame gameId={'SATOSHI_MAN'} />}
+            {selectedGame?.id === 'BRICK_BLITZ' && <TetrisGame gameId={'BRICK_BLITZ'} />}
+            {selectedGame?.id === 'MAZE_MUNCHER' && <SatoshiManGame gameId={'MAZE_MUNCHER'} />}
             {selectedGame?.id === 'FEAST_OR_FAMINE' && <FeastFamine gameId={'FEAST_OR_FAMINE'} />}
           </WalletProvider>
         </Suspense>
